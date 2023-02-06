6a7bd4322bd6ce5d251aaddc342202d4117d7d93
Kevin Nguyen <nguyen.keviinn@gmail.com>
2023-02-06 15:50:12 -0500

    First Commit

--- src/exercise1/Card.java
+++ src/exercise1/Card.java
@@ -7,8 +7,8 @@ package exercise1;
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
- * @author dancye
- * @author Paul Bonenfant May 2020
+ * @author Kevin Nguyen
+ * @Date February 6, 2023
  */
 public class Card {
 
@@ -43,4 +43,8 @@ public class Card {
     public void setValue(int value) {
         this.value = value;
     }  
+
+    void setValue(String hearts) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
